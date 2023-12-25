124e43ea87e0759c32bcf5f932362e482cbc7c8b
Meenu <Meenu@LAPTOP-TOE98MBN>
2023-12-25 16:51:14 +0530

    1 barri

---
+++ 23-12-2023/src/gitcammand/classgit.java
@@ -0,0 +1,22 @@
+package gitcammand;
+
+public class classgit 
+{
+	public void m1()
+	{
+		System.out.println("this is first method m1");
+		
+	}
+	public void m2()
+	{
+		System.out.println("this is second method m2");
+	}
+	public static void main(String[] args) 
+	{
+	classgit obj=new classgit();
+	obj.m1();
+	obj.m2();
+		
+	}
+
+}
